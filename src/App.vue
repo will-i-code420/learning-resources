db6fbac1bf7ed450918258576d4c7a2b19f64409
Will Aronow <waronow123@gmail.com>
2022-01-13 21:08:00 -0700

    moved learning resources to own folder/component to clear up app. added basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,30 @@
 <template>
-  <ul>
-    <learning-resource
-      v-for="resource in resources"
-      :key="resource.id"
-      :resource="resource"
-    ></learning-resource>
-  </ul>
+  <saved-resources></saved-resources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import SavedResources from './components/learning-resources/SavedResources.vue';
 
 export default {
   components: {
-    LearningResource,
+    SavedResources,
   },
   data() {
-    return {
-      resources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'Official Vue.js Documentation',
-          link: 'https://v3.vuejs.org',
-        },
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'Official Vue.js Documentation',
-          link: 'https://v3.vuejs.org',
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
 </style>
--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,12 +3,12 @@
     <div>
       <header>
         <h2>{{ resource.title }}</h2>
-        <button>Delete</button>
+        <button @click="deleteResource(resource.id)">Delete</button>
       </header>
     </div>
     <p>{{ resource.description }}</p>
     <nav>
-      <a :href="resource.link">Learn More</a>
+      <a :href="resource.link" target="_blank">Learn More</a>
     </nav>
   </li>
 </template>
@@ -21,5 +21,42 @@ export default {
       required: true,
     },
   },
+  methods: {
+    deleteResource(id) {
+      this.$emit('delete-resource', id);
+    },
+  },
 };
 </script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h2 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
